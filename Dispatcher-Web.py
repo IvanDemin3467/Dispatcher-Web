5ef1dc9902a4c36757ade0954e650c309ec99506
IvanDemin3467 <IvanDemin3467@gmail.com>
2021-08-29 09:32:29 +0600

    Comments and exceptions handling

--- Dispatcher-Web.py
+++ Dispatcher-Web.py
@@ -1,55 +1,56 @@
-from flask import Flask, render_template, request, url_for, flash, redirect, session
-from werkzeug.exceptions import abort
-
-from os import environ
-
 import google.oauth2.credentials
-
+from datetime import datetime
+from flask import Flask, render_template, request, url_for, flash, redirect, session
+from google_auth_oauthlib.flow import *
+from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google_auth_oauthlib.flow import *
-
-from datetime import datetime
-
-# from waitress import serve
-
+from os import environ
+from werkzeug.exceptions import abort
 
 CLIENT_SECRETS_FILE = "client_secret.json"
 
-# This access scope grants read-only access to the authenticated user's Calendar
-# and Spreadsheets accounts.
+# This access scope grants read-only access to the authenticated user's Calendar  and Spreadsheets accounts.
 SCOPES = ['https://www.googleapis.com/auth/calendar',
           'https://www.googleapis.com/auth/spreadsheets']
-API_SERVICE_NAME = 'calendar'
-API_VERSION = 'v3'
 
 # periods_dict translates hour of the day into period of the day
 periods_dict = {"08": 1, "09": 2, "10": 2, "11": 3, "12": 3, "13": 4,
                 "14": 4, "15": 5, "16": 5, "17": 6, "18": 7, "19": 7}
-# days_dict translates day of the week into locl name of the day
+# days_dict translates day of the week into local name of the day
 days_dict = {0: "Пн", 1: "Вт", 2: "Ср", 3: "Чт", 4: "Пт", 5: "Сб", 6: "Вс"}
 
 
 # timetable stores all scheduled events (pairs)
 class Timetable:
     def __init__(self, name="Other"):
+        """
+        Initializes list of events as 3-d array
+        Also stores name of timetable
+        """
         self.timetable = [[["" for period in range(7)]
                            for day in range(7)]
-                          for week in range(52)]
+                          for week in range(53)]
         self.name = name
 
     def put(self, value, period, day, week):
+        """
+        Store the given value in the timetable.
+        Notices the error if there are two events in the same period
+        """
         stored_value = self.get(period, day, week)
-        self.timetable[week - 1][day - 1][period - 1] = value if stored_value == "" else "ОШИБКА! Две пары в одно время"
+        error_value = "ОШИБКА! Две пары в одно время: " + stored_value + "; " + value
+        self.timetable[week - 1][day - 1][period - 1] = value if stored_value == "" else error_value
 
     def get(self, period, day, week):
         return self.timetable[week - 1][day - 1][period - 1]
 
     def get_list(self):
-        # .get_list() - prepares list to load into spreadsheet
+        """
+        It prepares list of cells to load into spreadsheet
+        """
         complete_list = []
-        for week in range(52):
+        for week in range(53):
             empty_week = True
             complete_list.append(["Неделя: " + str(week + 1), "", "", "", "", "", "", ""])
             complete_list.append(["", "Пара 1", "Пара 2", "Пара 3", "Пара 4", "Пара 5", "Пара 6", "Пара 7"])
@@ -64,18 +65,18 @@ class Timetable:
                     row.append(value)
 
                 complete_list.append(row)
-            if empty_week:
-                del complete_list[-9:]
+            if empty_week: del complete_list[-9:]  # Do not print empty weeks
         return complete_list
 
 
 def load_into_spreadsheet(service, list_timetable):
-    # Input is: spreadsheet service, options dict and timetable object
-    # Call the Sheets API, creates new spreadsheet
-    # and loads data from timetable into spreadsheet
-    # Outputs link to created spreadsheet
+    """
+    Input is: spreadsheet service and list of timetable objects
+    It calls the Sheets API, creates new spreadsheets and loads data from timetables into spreadsheets
+    Outputs list of links to created spreadsheets
+    """
 
-    # list of urls to ouput
+    # list of urls to output
     global urls
     urls = []
 
@@ -88,11 +89,10 @@ def load_into_spreadsheet(service, list_timetable):
                 'title': timetable.name + datetime.now().strftime("-%Y-%m-%d-%H-%M-%S")
             }
         }
-        spreadsheet = service.spreadsheets().create(body=spreadsheet,
-                                                    fields='spreadsheetId').execute()
+        spreadsheet = service.spreadsheets().create(body=spreadsheet, fields='spreadsheetId').execute()
         spreadsheet_id = spreadsheet.get('spreadsheetId')
 
-        # Get into first list
+        # Get into the first page in a spreadsheet
         range_name = "Лист1!A1"
 
         # Load values from timetable in appropriate format for spreadsheet
@@ -107,18 +107,23 @@ def load_into_spreadsheet(service, list_timetable):
             spreadsheetId=spreadsheet_id, range=range_name,
             valueInputOption=value_input_option, body=body).execute()
 
-        # How many calls updated?
+        # How many cells updated?
         flash('{0} cells updated.'.format(result.get('updatedCells')))
 
         # The link to the result
         url = "https://docs.google.com/spreadsheets/d/" + spreadsheet_id
         flash(url)
 
+        # prepare the output
         urls.append({timetable.name.split("@")[0]: url})
     return urls
 
 
-def list_events_by_guest(service, options):
+def list_events_by_guest(service, query):
+    """
+    Input is Google Calendar service and parameters of query
+    It makes API call to Calendar , gets all events from all calendars and filters them according to query
+    """
     # Same as byparam, but makes timetable for given guest (tutor)
     # lists all events in main calendar which are between two dates, given in options
     # Also it filters events by name of guest (tutor)
@@ -144,30 +149,36 @@ def list_events_by_guest(service, options):
             for event in events['items']:
                 try:
                     event_name = event['summary']
-                    event_start = datetime.strptime(event['start']['dateTime'],
-                                                    "%Y-%m-%dT%H:%M:%S+06:00")
-                    for timetable in list_timetable:
-                        if options["lower_date"] < event_start < options["upper_date"]:
-                            attendees = event['attendees']
-                            for attendee in attendees:
-                                if attendee['email'] == timetable.name:
-                                    count_events += 1
-                                    week = int(event_start.strftime("%W").lstrip("0"))
-                                    week = week - options["first_week"]
-                                    day = int(event_start.strftime("%w"))
-                                    period = event_start.strftime("%H")
-                                    try:
-                                        period = periods_dict[period]
-                                    except:
-                                        period = "other"
-                                    data = event_name
-                                    # print(data)
-                                    try:
-                                        timetable.put(value=data, period=period, day=day, week=week)
-                                    except:
-                                        print("error while put()")
+                    event_start = datetime.strptime(event['start']['dateTime'], "%Y-%m-%dT%H:%M:%S+06:00")
                 except:
-                    pass
+                    break
+                for timetable in list_timetable:
+                    if query["lower_date"] < event_start < query["upper_date"]:
+                        attendees = event['attendees']
+                        for attendee in attendees:
+                            if attendee['email'] == timetable.name:
+                                count_events += 1
+                                try:
+                                    week = int(event_start.strftime("%W").lstrip("0"))
+                                    week = week - query["first_week"]
+                                except:
+                                    week = 0
+                                    flash(timetable.name + ". Error with week")
+                                try:
+                                    day = int(event_start.strftime("%w"))
+                                except:
+                                    day = 0
+                                    flash(timetable.name + ". Error with day")
+                                try:
+                                    period = event_start.strftime("%H")
+                                    period = periods_dict[period]
+                                except:
+                                    period = 7
+                                    flash(timetable.name + ". Error with period")
+                                try:
+                                    timetable.put(value=event_name, period=period, day=day, week=week)
+                                except:
+                                    flash(timetable.name + ". Error while put()")
 
             page_token = events.get('nextPageToken')
             if not page_token:
